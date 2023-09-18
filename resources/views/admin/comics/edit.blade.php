3e4630dc8a8d7713a07abbc52f151934cb90f4d7
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-09-18 17:28:33 +0200

    ADD update page with validations

--- resources/views/admin/comics/edit.blade.php
+++ resources/views/admin/comics/edit.blade.php
@@ -17,45 +17,80 @@
 
         <div class="right w-50 p-5">
             <div class="mb-3">
-                <label for="title" class="form-label">Title</label>
-                <input type="text" name="title" class="form-control" id="title" aria-describedby="emailHelp" required value="{{ $comic->title }}">
+                <label for="title" class="form-label">Title<span class="text-danger">*</span></label>
+                <input type="text" name="title" class="form-control @error('title') is-invalid @enderror" id="title" aria-describedby="emailHelp" value="{{ old('title', $comic->title) }}">
+                @error('title')
+                <div class="alert alert-danger my-2">
+                    {{ $message }}
+                </div> 
+                @enderror
             </div>
             <div class="mb-3">
-                <label for="descr" class="form-label">Description</label>
-                <input type="text" name="description" class="form-control" id="descr" aria-describedby="emailHelp" required value="{{ $comic->description }}">
+                <label for="descr" class="form-label">Description<span class="text-danger">*</span></label>
+                <input type="text" name="description" class="form-control @error('description') is-invalid @enderror" id="descr" aria-describedby="emailHelp" value="{{ old('description', $comic->description) }}">
+                @error('description')
+                <div class="alert alert-danger my-2">
+                    {{ $message }}
+                </div> 
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="thumb" class="form-label">Image Link</label>
-                <input type="text" name="thumb" class="form-control" id="thumb" aria-describedby="emailHelp" value="{{ $comic->thumb }}">
+                <input type="text" name="thumb" class="form-control @error('thumb') is-invalid @enderror" id="thumb" aria-describedby="emailHelp" value="{{ old('thumb', $comic->thumb) }}">
+                @error('thumb')
+                <div class="alert alert-danger my-2">
+                    {{ $message }}
+                </div> 
+                @enderror
             </div>
             <div class="mb-3">
-                <label for="price" class="form-label">Price</label>
-                <input type="number" name="price" step="any" class="form-control" id="price" aria-describedby="emailHelp" required value="{{ $comic->price }}">
+                <label for="price" class="form-label">Price<span class="text-danger">*</span></label>
+                <input type="number" name="price" step="any" class="form-control @error('price') is-invalid @enderror" id="price" aria-describedby="emailHelp" value="{{ old('price', $comic->price) }}">
+                @error('price')
+                <div class="alert alert-danger my-2">
+                    {{ $message }}
+                </div> 
+                @enderror
             </div>
 
-            <button type="submit" class="btn btn-warning">Edit</button>
+            <button type="submit" class="btn btn-primary">Add</button>
         </div>
 
         <div class="left w-50 p-5">
             <div class="mb-3">
                 <label for="date" class="form-label">Sale Date</label>
-                <input type="date" name="sale_date" class="form-control" id="date" aria-describedby="emailHelp" required value="{{ $comic->sale_date }}">
+                <input type="date" name="sale_date" class="form-control @error('sale_date') is-invalid @enderror" id="date" aria-describedby="emailHelp" value="{{ old('sale_date', $comic->sale_date) }}">
+                @error('sale_date')
+                <div class="alert alert-danger my-2">
+                    {{ $message }}
+                </div> 
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="series" class="form-label">Series</label>
-                <input type="text" name="series" class="form-control" id="series" aria-describedby="emailHelp" required value="{{ $comic->series }}">
+                <input type="text" name="series" class="form-control @error('series') is-invalid @enderror" id="series" aria-describedby="emailHelp" value="{{ old('series', $comic->series) }}">
+                @error('series')
+                <div class="alert alert-danger my-2">
+                    {{ $message }}
+                </div> 
+                @enderror
             </div>
             <div class="mb-3">
-                <label for="type" class="form-label">Type</label>
-                <input type="text" name="type" class="form-control" id="type" aria-describedby="emailHelp" required value="{{ $comic->type }}">
+                <label for="type" class="form-label">Type<span class="text-danger">*</span></label>
+                <input type="text" name="type" class="form-control @error('type') is-invalid @enderror" id="type" aria-describedby="emailHelp" value="{{ old('type', $comic->type) }}">
+                @error('type')
+                <div class="alert alert-danger my-2">
+                    {{ $message }}
+                </div> 
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="artists" class="form-label">Artists</label>
-                <input type="text" name="artists" class="form-control" id="artists" aria-describedby="emailHelp" required value="{{ is_array($comic->artists) ? implode(', ', $comic->artists) : $comic->artists }}">
+                <input type="text" name="artists" class="form-control" id="artists" aria-describedby="emailHelp" value="{{ old('artists', $comic->artists) }}">
             </div>
             <div class="mb-3">
                 <label for="writers" class="form-label">writers</label>
-                <input type="text" name="writers" class="form-control" id="writers" aria-describedby="emailHelp" required value="{{ is_array($comic->writers) ? implode(', ', $comic->writers) : $comic->writers }}">
+                <input type="text" name="writers" class="form-control" id="writers" aria-describedby="emailHelp" value="{{ old('writers', $comic->writers) }}">
             </div>
         </div>
     
